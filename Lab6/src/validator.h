e097040792cec8611df506bdae843eab3d1fa360
Roman <Sofronov2@yandex.ru>
2013-05-19 17:17:02 +0400

    Revert "Revert "5/6 labs""
    This reverts commit 42971ad99465d24e534f93408567318ecfbb032d.

---
+++ Lab6/src/validator.h
@@ -0,0 +1,13 @@
+/*
+ * validator.h
+ *
+ *  Created on: 24.03.2013
+ *      Author: roman
+ */
+
+#ifndef VALIDATOR_H_
+#define VALIDATOR_H_
+
+int isAvaliable(char *s);
+
+#endif /* VALIDATOR_H_ */
